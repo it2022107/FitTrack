ec7feab7b12c55242fd92beb399d676662b446db
it2022107 <it2022107@hua.gr>
2025-11-24 15:15:47 +0200

    first commit

---
+++ src/main/java/gr/hua/dit/Fit_Track/FitTrackApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.Fit_Track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackApplication.class, args);
+	}
+
+}
