b682682fa35c27700843b2a51592dd819c0932e3
it2022107 <it2022107@hua.gr>
2025-12-13 15:46:48 +0200

    Used external service(only the Sms service), external service link:https://github.com/gkoulis/DS-Lab-NOC

---
+++ src/main/java/gr/hua/dit/Fit_Track/core/port/SmsNotificationPort.java
@@ -0,0 +1,4 @@
+package gr.hua.dit.Fit_Track.core.port;
+
+public class SmsNotificationPort {
+}
---
+++ src/main/java/gr/hua/dit/Fit_Track/core/port/impl/dto/SendSmsRequest.java
@@ -0,0 +1,4 @@
+package gr.hua.dit.Fit_Track.core.port.impl.dto;
+
+public record SendSmsReqiest() {
+}
