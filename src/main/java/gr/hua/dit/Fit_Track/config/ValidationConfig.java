21289868ca3012902660f8000988c3a8f3a748d4
it2022107 <it2022107@hua.gr>
2026-01-01 15:36:36 +0200

    Added validation and post construct for db

---
+++ src/main/java/gr/hua/dit/Fit_Track/config/ValidationConfig.java
@@ -0,0 +1,4 @@
+package gr.hua.dit.Fit_Track.config;
+
+public class ValidationConfig {
+}
---
+++ src/main/java/gr/hua/dit/Fit_Track/core/service/model/InitializationService.java
@@ -0,0 +1,4 @@
+package gr.hua.dit.Fit_Track.core.service.model;
+
+public class InitializationService {
+}
